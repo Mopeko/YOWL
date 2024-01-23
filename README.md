b36a5aa2a3c177159dd48743d4f842d4254ece75
Mayeulgras <mayeul.gras@epitech.digital>
2024-01-23 17:25:16 +0100

    test

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # YOWL
+
+
+hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
--- suits_react/src/pages/home.js
+++ suits_react/src/pages/home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import NavBar from '../components/navbar';
 import TopBar from '../components/topbar';
+import PostHome from '../components/postHome';
 
 const Home = () => {
   return (
     <div>
       {/* Contenu de votre application */}
+      <PostHome />
       <TopBar />
       <NavBar />
     </div>
